8a16c665f6b89cee3a1c3f69af862680d68acd93
Marcin Bojara <marcin_5@mail.ru>
2025-07-15 12:40:33 +0200

    Add autoloader implementation for dynamic class loading in Redis namespace

---
+++ app/autoloader.php
@@ -0,0 +1,11 @@
+<?php
+
+spl_autoload_register(function ($class) {
+    // Convert namespace to file path
+    $class = str_replace('Redis\\', '', $class);
+    $file = __DIR__ . '/src/' . str_replace('\\', '/', $class) . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
